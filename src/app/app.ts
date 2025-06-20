5511085d030119cae5521e6738e1b4b161e75a67
Michał Mrózek <pinguinv2206@gmail.com>
2025-06-20 15:02:59 +0200

    Added button to add new habits

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, computed, inject, OnInit } from '@angular/core';
+import { RouterModule, Router, NavigationEnd } from '@angular/router';
+
+import { toSignal } from '@angular/core/rxjs-interop';
+import { filter, map } from 'rxjs';
 
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
-  imports: [RouterModule, MatTabsModule, MatButtonModule],
+  imports: [RouterModule, MatTabsModule, MatButtonModule, MatIcon],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {}
+export class App implements OnInit {
+  private router = inject(Router);
+
+  // why: to show or hide 'add habit button'
+  protected routeSignal = toSignal(
+    this.router.events.pipe(
+      filter((e) => e instanceof NavigationEnd),
+      map((e: NavigationEnd) => e.urlAfterRedirects)
+    )
+  );
+
+  protected showButton = computed(() => this.routeSignal() === '/');
+
+  ngOnInit(): void {
+    // remove preload class with plain JS
+    setTimeout(() => {
+      document.body.classList.remove('preload');
+    }, 500);
+  }
+}
