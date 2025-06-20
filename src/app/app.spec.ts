357a0f82f41d7c7f278f5570be098ea2e56ee72c
Michał Mrózek <pinguinv2206@gmail.com>
2025-06-20 17:16:22 +0200

    Added few app component tests
    (for the moment I don't really know how to test or what to test inside components so there is that ;-; )

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,19 +1,41 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+
 import { App } from './app';
+import { appRoutes } from './app.routes';
+import { By } from '@angular/platform-browser';
 
 describe('App', () => {
+  let fixture: ComponentFixture<App>;
+  let component: App;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [provideRouter(appRoutes)],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(App);
+    fixture.detectChanges();
+    component = fixture.componentInstance;
+  });
+
+  it('should create', () => {
+    expect(component).toBeDefined();
   });
 
   it('should display app name', () => {
-    const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
       'Habits Tracker v2'
     );
+  });
+
+  describe('button[matFab]', () => {
+    it('should create', () => {
+      expect(
+        fixture.debugElement.query(By.css('button[matFab]'))
+      ).toBeDefined();
+    });
   });
 });
