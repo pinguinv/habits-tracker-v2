6baa0033e3eea55fa19a60442b806c474234d400
Michał Mrózek <pinguinv2206@gmail.com>
2025-06-20 10:40:42 +0200

    Added routing
    Routing with eager loading for landing page and lazy loading for others
    Generated needed components for routing
    Redirecting to 'habits' page from unknown locations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Route } from '@angular/router';
+import { Habits } from './components/habits/habits';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: Habits,
+  },
+  {
+    path: 'achievements',
+    loadComponent: () =>
+      import('./components/achievements/achievements').then(
+        (c) => c.Achievements
+      ),
+  },
+  {
+    path: 'charts',
+    loadComponent: () =>
+      import('./components/charts/charts').then((c) => c.Charts),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
---
+++ src/app/components/habits/habits.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-habits',
+  imports: [CommonModule],
+  templateUrl: './habits.html',
+  styleUrl: './habits.css'
+})
+export class Habits {}
